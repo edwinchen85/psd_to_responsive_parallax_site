1f70f2ec7086c8c18e2eb2222d826d40e47620c5
edwinchen85 <edwinchen85@gmail.com>
2017-03-07 11:59:34 +0800

    Add markup and style for testimonial section

--- js/main.js
+++ js/main.js
@@ -53,6 +53,33 @@ $(document).ready(function() {
 
   });
 
+
+  $('.testimonial-carousel').carouFredSel({
+
+    responsive: true,
+    width: '100%',
+    circular: true,
+    scroll: {
+      items: 1,
+      duration: 500,
+      pauseOnHover: true
+    },
+    auto: true,
+    items: {
+      visible: {
+        min: 1,
+        max: 1
+      },
+      height: "variable"
+    },
+    pagination: {
+      container: ".testipager",
+      anchorBuilder: false
+    }
+
+  });
+
+
   $(window).scroll(function() {
 
     var top = $(window).scrollTop();
