00fb0cbb581581b05c6b8ffff7bd5fc674293d47
edwinchen85 <edwinchen85@gmail.com>
2017-03-07 14:51:44 +0800

    Fix carousel functionality at portfolio section

--- js/main.js
+++ js/main.js
@@ -29,6 +29,29 @@ $(document).ready(function() {
 
   });
 
+  $('.portfolio-carousel').carouFredSel({
+
+    responsive: true,
+    width: '100%',
+    circular: true,
+    prev: '#prev',
+    next: '#next',
+    scroll: {
+      items: 1,
+      duration: 500,
+      pauseOnHover: true
+    },
+    auto: true,
+    items: {
+      visible: {
+        min: 1,
+        max: 4
+      },
+      height: "variable"
+    },
+
+  });
+
 
   $('.team-carousel').carouFredSel({
 
